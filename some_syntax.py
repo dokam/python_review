c5782b16d335736a7e577ca17674410ef21f56c7
Douglas <kaminskidk@gmail.com>
2023-01-22 07:32:47 +0000

    following tutorial some calcs

---
+++ some_syntax.py
@@ -0,0 +1,26 @@
+# this is a first comment.
+text = "# This is not a comment because it's inside quotes."
+print(text)
+
+print("\nSome calculation:")
+
+print(5/2)
+print("\nDivision always bring a float number 8/5:")
+print(8/5)
+
+print(19/3)
+
+print("\ndivision with no decimal places 5//3:")
+print(8//3)
+
+print("\nCalculation of Rectangle's area:")
+width = 5
+height =3
+print(width*height)
+
+print("\n Rounding values:")
+
+tax = 12.5/100
+price = 100.50
+result = price*tax
+print(round(result,2))
