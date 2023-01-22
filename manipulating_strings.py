17b4dd5433ead396a2217fd958d2ad6ec8ffd017
Douglas <kaminskidk@gmail.com>
2023-01-22 08:06:23 +0000

    starting strings

---
+++ manipulating_strings.py
@@ -0,0 +1,9 @@
+print('spam eggs')
+
+print('doesn\'t') #to scape ' use \'
+
+print("doesn't") #or use double quotes instead
+
+print('\ntest') #\n means no line
+
+print(r'C:\some\name') #user raw strings to avoid jumping to next line
