a248dc11e30835118dadcb2abe21790c654c9ef3
Douglas <kaminskidk@gmail.com>
2023-01-23 07:36:05 +0000

    strings and starting lists

---
+++ lists_operation.py
@@ -0,0 +1,4 @@
+#list operations
+
+listValues = squares = [1, 4, 9, 16, 25]
+print(listValues)
--- manipulating_strings.py
+++ manipulating_strings.py
@@ -7,3 +7,22 @@ print("doesn't") #or use double quotes instead
 print('\ntest') #\n means no line
 
 print(r'C:\some\name') #user raw strings to avoid jumping to next line
+
+valueStr = 'test'
+
+print(3 * valueStr)
+
+#printing long strings
+
+print('Put several strings with parentheses '
+	 'to have them joined together.')
+
+#calling index of strings in multiple ways
+
+print(valueStr[0] + valueStr[1])
+
+print(valueStr[-1] + valueStr[-2])
+
+#len brings the lenght of a string
+print(len(valueStr))
+
