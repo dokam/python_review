e0b7252247e26b5397ffff55e917503e90ec7a84
Douglas <kaminskidk@gmail.com>
2023-01-23 07:51:36 +0000

    lists cont.

--- lists_operation.py
+++ lists_operation.py
@@ -1,4 +1,20 @@
 #list operations
 
-listValues = squares = [1, 4, 9, 16, 25]
-print(listValues)
+listValuesA = [1, 4, 9, 16, 25]
+print(listValuesA)
+
+#printing by index
+print(listValuesA[1])
+
+#concatenate
+listValuesB = [2, 82, 87, 16, 25]
+print(listValuesA + listValuesB)
+
+listValuesC = [2, 82, 87, 16, 25]
+#changing a value - lists are mutable
+listValuesC[0] = 44
+print(listValuesC)
+
+#adding values to the end of a list
+listValuesC.append(50)
+print(listValuesC)
