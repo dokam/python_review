9632b49550d92872f2478aad03c449ca0b6b78d1
Douglas <kaminskidk@gmail.com>
2023-01-29 18:31:33 +0000

    starting statements

---
+++ statements.py
@@ -0,0 +1,11 @@
+x = int(input("Please an integer:"))
+
+if x < 0 :
+	x = 0
+	print ("When negative it's changed to zero.")
+elif x == 0:
+	print('Zero.')
+elif x == 1:
+    print('Single.')
+else:
+	print('More.')
