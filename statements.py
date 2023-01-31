4dada4aa3c47afcc2aff3e6e1b1ceb83878ea141
Douglas <kaminskidk@gmail.com>
2023-01-31 16:56:56 +0000

    more statements examples.

--- statements.py
+++ statements.py
@@ -50,3 +50,13 @@ for i in range(len(a)):
 print('\n')
 # iterable
 print(sum(range(4)))
+
+print('\n')
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+        else:
+            print(n, 'is a prime number!')
