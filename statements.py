2e64dcaac1952ded3eca22d92dc527937a0f0d87
Douglas <kaminskidk@gmail.com>
2023-02-08 07:33:03 +0000

    more statements examples.

--- statements.py
+++ statements.py
@@ -60,3 +60,21 @@ for n in range(2, 10):
             break
         else:
             print(n, 'is a prime number!')
+
+print('\n')
+
+from enum import Enum
+class Color(Enum):
+    RED = 'red'
+    GREEN = 'green'
+    BLUE = 'blue'
+
+color = Color(input("Enter your choice of 'red', 'blue' or 'green': "))
+
+match color:
+    case Color.RED:
+        print("I see red!")
+    case Color.GREEN:
+        print("Grass is green")
+    case Color.BLUE:
+        print("I'm feeling the blues :(")
